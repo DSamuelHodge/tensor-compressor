a5b866282339883b8a35ebfcd627421aea047510
DSamuelHodge <41060830+DSamuelHodge@users.noreply.github.com>
2025-01-31 01:36:01 -0500

    Add evaluation module init

---
+++ tensor_compressor/evaluation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Evaluation module for tensor compression
+"""
+
+from .evaluator import ModelEvaluator, ComprehensiveEvaluator, EvaluationMetrics
+
+__all__ = ['ModelEvaluator', 'ComprehensiveEvaluator', 'EvaluationMetrics']
